126b7fead904331cbc670cbd326851ec4102d923
Arastun123 <ekbr03@gmail.com>
2024-06-23 20:26:41 +0400

    Show datas

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import { View, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+}
+
+function CategoriesScreen() {
+    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+}
+
+export default CategoriesScreen;
